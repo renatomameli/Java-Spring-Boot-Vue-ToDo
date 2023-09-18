a1315aadb431a0b0dbd1132b6cd8471a9fa2e79d
renato.mameli <renato.mameli@de.rhenus.com>
2023-09-18 16:26:49 +0200

    Backend security stuff

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,7 +31,13 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
 
     // Utils
-    implementation("org.apache.commons:commons-lang3:3.0")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<Test> {
